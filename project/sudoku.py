0dec09ab240462f986ef699cd0d16cdfe85f2ade
frankbryce <jonnyjack7@gmail.com>
2022-03-12 17:42:37 -0500

    Adding a method for generating a starting point for a sudoku grid.
    Added a few tests to debug the errors I was seeing.

--- project/sudoku.py
+++ project/sudoku.py
@@ -12,12 +12,15 @@ class Cell:
     grid: Grid  # grid this cell is in
     row: int  # 0-indexed
     col: int  # 0-indexed
-    val: int = 0  # 0 indicates cell is empty. valid values are 1-9
+    box: int  # 0-indexed
+    val: int  # 0 indicates cell is empty. valid values are 1-9
 
-    def __init__(self, grid, row, col):
+    def __init__(self, grid, row, col, val=0):
         self.grid = grid
         self.row = row
         self.col = col
+        self.box = (row//3) + 3*(col//3)
+        self.val = val
 
     def Box(self) -> list[int]:
         rst = (self.row//3)*3
@@ -44,8 +47,8 @@ class Grid:
         return [self.cells[i][col] for i in range(9)]
 
     def Box(self, box) -> list[int]:
-        rst = box//3
-        cst = box % 3
+        rst = (box//3)*3
+        cst = (box%3)*3
         return [self.cells[rst+r][cst+c] for r in range(3) for c in range(3)]
 
     @staticmethod
@@ -65,6 +68,14 @@ class Grid:
         ret.solved = [[ret.cells[r][c].val for c in range(9)] for r in range(9)]
         return ret
 
+    def From(ints: list[list[int]]) -> Grid:
+        ret = Grid()
+        ret.cells = [
+            [Cell(grid=ret, row=row, col=col, val=ints[row][col]) for col in range(9)]
+            for row in range(9)
+        ]
+        return ret
+
     @staticmethod
     def New(difficulty: int, nsims: int=10) -> Grid:
         ret = Grid.Full()
@@ -75,8 +86,50 @@ class Grid:
             random.shuffle(rclist)
             for rc in list(rclist):
                 cell = ret.cells[rc[0]][rc[1]]
+                if cell.row != rc[0] or cell.col != rc[1]:
+                    print("Houston, we have a problem")
                 if cell.val == 0:
                     continue
+
+                # check that this location can be logicked back in
+                logickable = False
+                # only place for num in Row, Col or Box?
+                for loc in [ret.Row(cell.row), ret.Col(cell.col), ret.Box(cell.box)]:
+                    found = False
+                    for _cell in loc:
+                        if _cell is cell:
+                            continue
+                        can_be_here = True
+                        for _other_cell in (ret.Row(_cell.row)+ret.Col(_cell.col)+ret.Box(_cell.box)):
+                            if _other_cell is _cell:
+                                continue
+                            if _other_cell.val == cell.val:
+                                can_be_here = False
+                                break
+                        if can_be_here:
+                            found = True
+                            break
+                    if not found:
+                        logickable = True
+                        break
+
+                # only num for Cell?
+                if not logickable: # yet
+                    sees = set()
+                    for _cell in (ret.Row(cell.row)+ret.Col(cell.col)+ret.Box(cell.box)):
+                        if _cell is cell:
+                            continue
+                        sees.add(_cell.val)
+                    sees.discard(0)
+                    if cell.val in sees:
+                        print("We have a problem, Houston")
+                    if len(sees) == 8:
+                        logickable = True
+
+                # we are removing a cell we can't logic back
+                if not logickable:
+                    continue
+
                 orig_val = cell.val
                 cell.val = 0
                 tot_tries = 0
@@ -88,8 +141,7 @@ class Grid:
                     while not cell_added and tries<2*difficulty:
                         tries += 1
                         tot_tries += 1
-                        good_strat = False
-                        if random.choice([1,2,3]) == 1:
+                        if random.choice([1,2]) == 1:
                             good_choice = False
                             while not good_choice:
                                 idx = random.choice(list(range(9)))
@@ -108,7 +160,7 @@ class Grid:
                                     for v in _cell.Box():
                                         vals.discard(v)
                                     if len(vals) == 1:
-                                        good_strat = True
+                                        cell_added = True
                                         break
                         else:
                             good_choice = False
@@ -128,16 +180,18 @@ class Grid:
                             for _cell in loc(idx):
                                 if _cell.val != 0:
                                     continue
+                                if _cell.val == num:
+                                    print("Houston, we have a problem")
                                 if (num not in _cell.Row() and
                                     num not in _cell.Col() and
                                     num not in _cell.Box()):
                                     num_pos += 1
-                            good_strat = (num_pos == 1)
-                        if good_strat:
-                            break
+                            cell_added = (num_pos == 1)
                 if tot_tries <= nsims*difficulty:
                     success = True
+                    break
                 else:
+                    print("added value back")
                     cell.val = orig_val
         return ret
         
---
+++ project/sudoku_test.py
@@ -0,0 +1,67 @@
+import sudoku
+import unittest
+
+class TestSudokuMethods(unittest.TestCase):
+
+    def testRow(self):
+        grid = sudoku.Grid.From([
+            [1,2,0,4,5,6,7,8,9],
+            [2,3,4,5,6,7,8,9,1],
+            [3,4,5,6,7,8,9,1,2],
+            [4,5,6,7,8,9,1,2,3],
+            [5,6,7,8,9,1,2,3,4],
+            [6,7,8,9,1,2,3,4,5],
+            [7,8,9,1,2,3,4,5,6],
+            [8,9,1,2,3,4,5,6,7],
+            [9,1,2,3,4,5,6,7,8]])
+        self.assertEqual(
+            [cell.val for cell in grid.Row(2)],
+            [3,4,5,6,7,8,9,1,2])
+        self.assertEqual(
+            [cell.row for cell in grid.Row(5)],
+            [5,5,5,5,5,5,5,5,5])
+        self.assertEqual(
+            [cell.col for cell in grid.Row(0)],
+            [0,1,2,3,4,5,6,7,8])
+
+    def testCol(self):
+        grid = sudoku.Grid.From([
+            [1,2,3,4,5,6,7,8,9],
+            [2,3,4,5,6,7,8,9,1],
+            [0,4,5,6,7,8,9,1,2],
+            [4,5,6,7,8,9,1,2,3],
+            [5,6,7,8,9,1,2,3,4],
+            [6,7,8,9,1,2,3,4,5],
+            [7,8,9,1,2,3,4,5,6],
+            [8,9,1,2,3,4,5,6,7],
+            [9,1,2,3,4,5,6,7,8]])
+        self.assertEqual(
+            [cell.val for cell in grid.Col(2)],
+            [3,4,5,6,7,8,9,1,2])
+        self.assertEqual(
+            [cell.col for cell in grid.Col(5)],
+            [5,5,5,5,5,5,5,5,5])
+        self.assertEqual(
+            [cell.row for cell in grid.Col(0)],
+            [0,1,2,3,4,5,6,7,8])
+
+    def testBox(self):
+        grid = sudoku.Grid.From([
+            [1,2,3,4,5,6,7,8,9],
+            [2,3,4,5,6,7,8,9,0],
+            [3,4,5,6,7,8,9,1,2],
+            [4,5,6,7,8,9,1,2,3],
+            [5,6,7,8,9,1,2,3,4],
+            [6,7,8,9,1,2,3,4,5],
+            [7,8,9,1,2,3,4,5,6],
+            [8,9,1,2,3,4,5,6,7],
+            [9,1,2,3,4,5,6,7,8]])
+        self.assertEqual(
+            [cell.val for cell in grid.Box(2)],
+            [7,8,9,8,9,0,9,1,2])
+        self.assertEqual(
+            [cell.col for cell in grid.Box(5)],
+            [6,7,8,6,7,8,6,7,8])
+        self.assertEqual(
+            [cell.row for cell in grid.Box(0)],
+            [0,0,0,1,1,1,2,2,2])
